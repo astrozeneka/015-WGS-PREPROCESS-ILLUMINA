869321b7f632e7a1abfede33b6a9ec5d2da97557
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2023-11-06 15:33:05 +0700

    Make it update the tmp file name

--- 204_kmerfreq_17_indiv.sh
+++ 204_kmerfreq_17_indiv.sh
@@ -10,8 +10,8 @@ indiv=$1
 export PATH=$PATH:~/softwares/kmerfreq/kmerfreq
 mkdir -p data/kmerfreq
 
-echo "" > read_files.lib.tmp
-echo "data/trimmed_illumina/${indiv}_1_pairend_trimmed.fq" >> read_files.lib.tmp
-echo "data/trimmed_illumina/${indiv}_2_pairend_trimmed.fq" >> read_files.lib.tmp
-kmerfreq -k 17 -t 32 -p data/kmerfreq/${indiv} read_files.lib.tmp
+echo "" > read_files_${indiv}.lib.tmp
+echo "data/trimmed_illumina/${indiv}_1_pairend_trimmed.fq" >> read_files_${indiv}.lib.tmp
+echo "data/trimmed_illumina/${indiv}_2_pairend_trimmed.fq" >> read_files_${indiv}.lib.tmp
+kmerfreq -k 17 -t 32 -p data/kmerfreq/${indiv} read_files_${indiv}.lib.tmp
 
